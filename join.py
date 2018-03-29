b87e9143504194c07b71bb637e2aaf32cabb8ac2
ipozdeev <igor.pozdeev@unisg.ch>
2018-03-28 21:34:16 -0400

    initial commit: concat implemented

---
+++ join.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from textables.tables import TexTable
+
+
+def concat(tup, axis=0, merge_columns=True):
+    """
+
+    Parameters
+    ----------
+    tup : list-like
+        of instances of `TexTable`
+    axis
+    merge_columns
+
+    Returns
+    -------
+    res : TexTable
+
+    """
+    if not merge_columns:
+        raise NotImplementedError("Not yet implemented.")
+
+    new_tbl = pd.concat([t.table_fmt for t in tup], axis=axis)
+
+    res = TexTable(new_tbl, fmt="{}")
+
+    return res
