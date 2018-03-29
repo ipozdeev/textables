65b823bdefddd7244d5e5f0a69b12bc1685c6aa2
ipozdeev <igor.pozdeev@unisg.ch>
2018-03-29 19:31:54 -0400

    initial commit

---
+++ examples/examples.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+
+import textables as tex
+
+
+def example_one():
+    """
+    """
+    df_coef = pd.DataFrame(np.random.normal(size=(2, 2)),
+                           index=["const", "beta"],
+                           columns=["x1", "x2"])
+
+    df_ts = pd.DataFrame(np.abs(np.random.normal(size=(2, 2))),
+                         index=["const", "beta"],
+                         columns=["x1", "x2"])
+
+    df_r2 = pd.DataFrame(np.array([[0.5, 0.75]]),
+                         index=["adj. R2"],
+                         columns=["x1", "x2"])
+
+    tbl_coef = tex.TexTable(df_coef, fmt="{:.4f}")
+    tbl_ts = tex.TexTable(df_ts, fmt="({:.2f})")
+    tbl_r2 = tex.TexTable(df_r2, fmt="{:.2f}")
+
+    tbl = tex.concat((tbl_coef.intertwine(tbl_ts), tbl_r2), axis=0)
+
+    tbl.with_dcolumn().to_tabularx(textwidth=0.75, x_column_loc=0,
+                                   buf="temp.tex")
+
+if __name__ == "__main__":
+    example_one()
