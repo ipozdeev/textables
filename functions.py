558aba733378a6e7e29f0826f7e7136022411a60
ipozdeev <igor.pozdeev@unisg.ch>
2018-03-29 18:56:41 -0400

    add to_diagonal

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import re
 
 
@@ -22,3 +23,31 @@ def deem_empty(cell):
             return np.isnan(cell)
         except TypeError:
             return False
+
+
+def to_diagonal(ser):
+    """Make diagonal table with na off-diagonal.
+
+    Parameters
+    ----------
+    ser : pandas.Series
+
+    Returns
+    -------
+
+    """
+    assert isinstance(ser, pd.Series)
+
+    # place `ser` on the diagonal main
+    df = pd.DataFrame(data=np.diag(ser),
+                      index=ser.index,
+                      columns=ser.index)
+
+    # put na off-diagonal
+    mask = pd.DataFrame(data=np.diag(np.ones((len(ser)))),
+                        index=ser.index,
+                        columns=ser.index).replace(0.0, np.nan).notnull()
+
+    df = df.where(mask)
+
+    return df
